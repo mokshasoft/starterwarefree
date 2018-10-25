cbee691d3d0a66f2ea620416c30f215870573776
Jonas Claeson <jonas.cl@protonmail.com>
2018-10-25 12:04:24 +0200

    The CMake build is working, except for the last objcopy

--- build/CMake/gcc.cmake
+++ build/CMake/gcc.cmake
@@ -8,6 +8,9 @@
 
 set(CMAKE_SYSTEM_NAME Generic)
 
+# Only needed for TI StarterWare code, not for the CMake toolchain.
+set(COMPILER gcc)
+
 if("${CROSS_COMPILER_PREFIX}" STREQUAL "")
     set(CROSS_COMPILER_PREFIX "arm-none-eabi-" CACHE INTERNAL "")
     message(STATUS "Using default value for CROSS_COMPILER_PREFIX (${CROSS_COMPILER_PREFIX})")
@@ -19,9 +22,8 @@ set(CMAKE_LD ${CROSS_COMPILER_PREFIX}ld)
 set(CMAKE_C_FLAGS "\
     -mcpu=cortex-a8 -mtune=cortex-a8 -march=armv7-a \
     -c \
-    -mlong-calls -fdata-sections -funsigned-char \
-    -ffunction-sections -Wall -Dgcc \
-    -D SUPPORT_UNALIGNED"
+    -mlong-calls -fdata-sections -funsigned-char -ffunction-sections -Wall \
+    -Dgcc -D SUPPORT_UNALIGNED -D MMCSD -DUARTCONSOLE -g -O2"
 )
 
 set(CMAKE_C_LINK_EXECUTABLE
@@ -34,6 +36,9 @@ set(CMAKE_C_LINK_EXECUTABLE
 )
 
 set(ARFLAGS "-c -r")
+set(CMAKE_C_ARCHIVE_CREATE "${CROSS_COMPILER_PREFIX}ar ${ARFLAGS} <TARGET> <LINK_FLAGS> <OBJECTS>")
+set(CMAKE_CXX_ARCHIVE_CREATE "${CROSS_COMPILER_PREFIX}ar ${ARFLAGS} <TARGET> <LINK_FLAGS> <OBJECTS>")
+
 set(BINFLAGS "-O binary")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
